66adab95b2c060f2af1a9a7fae48304a0d595f99
marianamartsy <mart7mariana@gmail.com>
2024-06-28 19:48:00 +0200

    styles changed

--- js/cards-animation.js
+++ js/cards-animation.js
@@ -1,6 +1,6 @@
 const objectInfoCards = document.querySelectorAll('.object-info__card')
 
-object-infoCards.forEach(card => {
+objectInfoCards.forEach(card => {
 	card.addEventListener('touchstart', function () {
 		this.classList.add('touch-active')
 	})
