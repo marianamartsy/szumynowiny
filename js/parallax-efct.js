738a31aa8f69cb72833e699d0725f0a361796e8d
marianamartsy <mart7mariana@gmail.com>
2024-07-01 14:55:01 +0200

    parallax edited

---
+++ js/parallax-efct.js
@@ -0,0 +1,32 @@
+function handleParallax() {
+	const scrollPosition = window.scrollY
+	const headerImage = document.querySelector('.header .parallax-img')
+	const heroImage = document.querySelector('.hero-img .parallax-img')
+
+	if (headerImage) {
+		headerImage.style.transform = 'translateY(' + scrollPosition * 0.5 + 'px)'
+	}
+
+	if (heroImage) {
+		const heroOffset = document.querySelector('.hero-img').offsetTop
+		if (scrollPosition > heroOffset) {
+			heroImage.style.transform = 'translateY(' + (scrollPosition - heroOffset) * 0.5 + 'px)'
+		} else {
+			heroImage.style.transform = 'translateY(0)'
+		}
+	}
+}
+
+handleParallax()
+let rafPending = false
+function scrollHandler() {
+	if (!rafPending) {
+		rafPending = true
+		requestAnimationFrame(function () {
+			handleParallax()
+			rafPending = false
+		})
+	}
+}
+
+document.addEventListener('scroll', scrollHandler)
