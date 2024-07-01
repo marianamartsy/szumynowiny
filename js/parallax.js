4685c060e1bf3e4b7fa8b9333cb4d4c8c1e0f14d
marianamartsy <mart7mariana@gmail.com>
2024-07-01 14:00:01 +0200

    parralax edited

--- js/parallax.js
+++ js/parallax.js
@@ -1,4 +1,18 @@
-const image = document.querySelectorAll('.thumbnail')
-new simpleParallax(image, {
-	orientation: 'down',
+document.addEventListener('scroll', function () {
+	const scrollPosition = window.scrollY
+	const headerImage = document.querySelector('.header .parallax-img')
+	const heroImage = document.querySelector('.hero-img .parallax-img')
+
+	if (headerImage) {
+		headerImage.style.transform = 'translateY(' + scrollPosition * 0.5 + 'px)'
+	}
+
+	if (heroImage) {
+		const heroOffset = document.querySelector('.hero-img').offsetTop
+		if (scrollPosition > heroOffset) {
+			heroImage.style.transform = 'translateY(' + (scrollPosition - heroOffset) * 0.5 + 'px)'
+		} else {
+			heroImage.style.transform = 'translateY(0)'
+		}
+	}
 })
