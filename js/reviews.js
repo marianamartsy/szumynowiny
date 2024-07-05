7fc9818ab4a05ec9ed5aee1e7906b8be01104611
marianamartsy <mart7mariana@gmail.com>
2024-07-05 21:45:05 +0200

    reviews edited

--- js/reviews.js
+++ js/reviews.js
@@ -1,16 +1,24 @@
-const reviewsDiv = document.querySelector('.reviews__google-user-ratings')
-
 async function fetchPlaceDetails() {
-	try {
-		const response = await fetch('https://szumynowiny-back.vercel.app/api/place-details')
-		const data = await response.json()
+    try {
+        const response = await fetch('https://szumynowiny-back.vercel.app/api/place-details');
+        const responseData = await response.text(); 
 
-		const { userRating, totalRatings } = data
+        console.log(responseData); 
+        let data;
+        try {
+            data = JSON.parse(responseData); 
+        } catch (error) {
+            console.error('Błąd parsowania JSON:', error);
+            return; 
+        }
 
-		reviewsDiv.textContent = `Ocena: ${userRating} z ${totalRatings || 0} opinii`
-	} catch (error) {
-		console.error('Błąd podczas pobierania danych z backendu:', error)
-	}
+        const { userRating, totalRatings } = data;
+
+        reviewsDiv.textContent = `Ocena: ${userRating} z ${totalRatings || 0} opinii`;
+    } catch (error) {
+        console.error('Błąd podczas pobierania danych z backendu:', error);
+    }
 }
 
-window.addEventListener('load', fetchPlaceDetails)
+window.addEventListener('load', fetchPlaceDetails);
+
