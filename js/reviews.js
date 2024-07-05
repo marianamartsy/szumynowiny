c48e4288abfe7eb2b376688cbef4b3667b00ebb9
marianamartsy <mart7mariana@gmail.com>
2024-07-05 21:45:42 +0200

    reviews edited

--- js/reviews.js
+++ js/reviews.js
@@ -1,24 +1,25 @@
+const reviewsDiv = document.querySelector('.reviews__google-user-ratings')
+
 async function fetchPlaceDetails() {
-    try {
-        const response = await fetch('https://szumynowiny-back.vercel.app/api/place-details');
-        const responseData = await response.text(); 
+	try {
+		const response = await fetch('https://szumynowiny-back.vercel.app/api/place-details')
+		const responseData = await response.text()
 
-        console.log(responseData); 
-        let data;
-        try {
-            data = JSON.parse(responseData); 
-        } catch (error) {
-            console.error('Błąd parsowania JSON:', error);
-            return; 
-        }
+		console.log(responseData)
+		let data
+		try {
+			data = JSON.parse(responseData)
+		} catch (error) {
+			console.error('Błąd parsowania JSON:', error)
+			return
+		}
 
-        const { userRating, totalRatings } = data;
+		const { userRating, totalRatings } = data
 
-        reviewsDiv.textContent = `Ocena: ${userRating} z ${totalRatings || 0} opinii`;
-    } catch (error) {
-        console.error('Błąd podczas pobierania danych z backendu:', error);
-    }
+		reviewsDiv.textContent = `Ocena: ${userRating} z ${totalRatings || 0} opinii`
+	} catch (error) {
+		console.error('Błąd podczas pobierania danych z backendu:', error)
+	}
 }
 
-window.addEventListener('load', fetchPlaceDetails);
-
+window.addEventListener('load', fetchPlaceDetails)
