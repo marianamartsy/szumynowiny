9ea508ebf3a6e946085ea8afdf636ff204bb0414
marianamartsy <mart7mariana@gmail.com>
2024-07-11 12:51:16 +0200

    maps and reviews edited

--- js/reviews.js
+++ js/reviews.js
@@ -1,4 +1,5 @@
 const reviewsDiv = document.querySelector('.reviews__google-user-ratings')
+const googleStarsDiv = document.querySelector('.reviews__google-stars')
 
 async function fetchPlaceDetails() {
 	try {
@@ -16,9 +17,27 @@ async function fetchPlaceDetails() {
 		const { userRating, totalRatings } = data
 
 		reviewsDiv.textContent = `Ocena: ${userRating} z ${totalRatings || 0} opinii`
+		updateStars(userRating, googleStarsDiv)
 	} catch (error) {
 		console.error('Błąd podczas pobierania danych z backendu:', error)
 	}
+}
+
+function updateStars(rating, container) {
+	const stars = container.querySelectorAll('.star')
+	const fullStars = Math.floor(rating)
+	const halfStar = rating % 1 >= 0.5 ? 1 : 0
+
+	stars.forEach((star, index) => {
+		star.style.display = 'inline-block'
+		star.classList.remove('full', 'half')
+
+		if (index < fullStars) {
+			star.classList.add('full')
+		} else if (index < fullStars + halfStar) {
+			star.classList.add('half')
+		}
+	})
 }
 
 window.addEventListener('load', fetchPlaceDetails)
