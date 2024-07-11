a4c9c85c2ba9429255a46dacef7e8ed0eec142d0
marianamartsy <mart7mariana@gmail.com>
2024-07-11 13:14:41 +0200

    maps and reviews edited

--- js/cards-animation.js
+++
@@ -1,7 +0,0 @@
-// const objectInfoCards = document.querySelectorAll('.object-info__card')
-
-// objectInfoCards.forEach(card => {
-// 	card.addEventListener('touchstart', function () {
-// 		this.classList.add('touch-active')
-// 	})
-// })
